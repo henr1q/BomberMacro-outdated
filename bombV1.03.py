75fdc0f51ac26e615e75556e6860ceee52d63d41
henr1q <69945767+henr1q@users.noreply.github.com>
2021-11-28 04:09:23 -0300

    Fix(?) for connect

--- bombV1.03.py
+++ bombV1.03.py
@@ -202,6 +202,9 @@ def check_errors():
                 pyautogui.hotkey('ctrl', 'f5')
                 ErrorsFound = ErrorsFound + 1
                 connect()
+                time.sleep(60)
+
+
 
 
 #Click next map
@@ -342,17 +345,17 @@ def threads():
         t3 = threading.Thread(target=bot)
 
     t4 = threading.Thread(target=show_stats)
-    t5 = threading.Thread(target=connect)
+    #t5 = threading.Thread(target=connect)
     t1.start()
     t2.start()
     t3.start()
     t4.start()
-    t5.start()
+    #t5.start()
     threadss.append(t1)
     threadss.append(t2)
     threadss.append(t3)
     threadss.append(t4)
-    threadss.append(t5)
+    #threadss.append(t5)
 
     for thread in threadss:
         thread.join()
